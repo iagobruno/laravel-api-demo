0e51054f5616dcea9572445a1e58cf914a8ffd04
Iago Bruno <iagobruno.dev@gmail.com>
2021-11-12 01:54:25 -0300

    feat: Install Pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,10 @@
 <?php
 
-namespace Tests\Feature;
+use function Pest\Laravel\{get, post, postJson};
+use Illuminate\Http\Response as StatusCode;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+test('Test landing page', function () {
+    get('/')
+        ->assertStatus(StatusCode::HTTP_OK)
+        ->assertSee('Laravel');
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,5 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
-}
+// test('1 + 1 = 2', function () {
+//     expect(1 + 1)->toBe(2);
+// });
